a07438edd46a813d8a53a9dda76336da6f534d10
hironakazuki <far_east_nightbird30@icloud.com>
2020-03-18 17:06:08 +0900

    separate graphql code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import client from './client';
+import { ME } from './graphql';
 
-const ME = gql`
-  query me {
-    user(login: "iteachonudemy") {
-      name
-      avatarUrl
-    }
-  }
-`
+class App extends Component {
+  render(){
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello, GrapQL!</div>
+        <Query query={ME}>
+          {
+            ({ loading, error, data }) => {
+              if(loading) return 'Loading...'
+              if(error) return `Error! ${error.message}`
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello, GrapQL!</div>
-      <Query query={ME}>
-        {
-          ({ loading, error, data }) => {
-            if(loading) return 'Loading...'
-            if(error) return `Error! ${error.message}`
-
-            return <div>{data.user.name}</div>
+              return <div>{data.user.name}</div>
+            }
           }
-        }
-      </Query>
-    </ApolloProvider>
-  );
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
